c6bcdcfad5e5abec7d08ca62c6ccd48570db5e7e
Sakshi Choube <sakshichoube4232gmail.com>
2024-05-08 01:59:35 +0530

    training pipeline added

---
+++ src/InsurancePricePrediction/pipelines/training_pipeline.py
@@ -0,0 +1,26 @@
+from src.InsurancePricePrediction.components.data_ingestion import DataIngestion
+
+from src.InsurancePricePrediction.components.data_transformation import DataTransformation
+
+from src.InsurancePricePrediction.components.model_trainer import ModelTrainer
+
+
+import os
+import sys
+from src.InsurancePricePrediction.logger import logging
+from src.InsurancePricePrediction.exception import customexception
+import pandas as pd
+
+
+obj=DataIngestion()
+
+train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+
+data_transformation = DataTransformation()
+
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+
+model_trainer=ModelTrainer()
+model_trainer.initate_model_training(train_arr,test_arr)
